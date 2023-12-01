c7a2f037313416a7bff9c13d7c751cf07c9b3d2a
Dustin Keovanna <cs15lfa23nk@ieng6-202.ucsd.edu>
2023-11-30 16:27:00 -0800

    changed grade

--- grade.sh
+++ grade.sh
@@ -8,6 +8,35 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+student_submission="student-submission/ListExamples.java"
+destination_directory="grading-area/"
+
+
+if [[ -f $student_submission ]]
+then
+    echo "File found"
+    cp -r "$student_submission" "$destination_directory"
+    cp -r TestListExamples.java "$destination_directory"
+else 
+    echo "File not found"
+    exit 1
+
+fi
+
+set +e
+
+
+# javac grading-area/ListExamples.java
+cd grading-area
+javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar *.java
+
+java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
+if [ $? -ne 0 ]; then
+  echo "compiling failed"
+  exit 1
+
+fi
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
